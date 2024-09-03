9ade2b29e02bc7ed55b697fed98c6e14871b02bb
Dabhi Vandan <ktiopyuoo@gmail.com>
2024-09-03 16:40:44 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,45 @@
+import { useState } from 'react'
+import "./App.css"
+import axios from 'axios';
+function App ()
+{
+  const[city,setcity]=useState();
+  const[weather,setweather]=useState();
+  const handlechange=(event)=>{
+    setcity(event.target.value)
+
+  }
+  const fetchweather = async () => {
+    try{
+      const response= await axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${'ddcb319e5ef46f8a6c79939233303290'}`)
+      setweather(response);
+      console.log(response);
+    }
+  
+    catch(error){
+      console.log("error fetching data",error);
+      
+    }
+
+  }
+  const getweather=()=>{
+    fetchweather();
+  }
+  return (
+    
+      <div className="container">
+        <input type="text" placeholder='Enter city name'  value={city} onChange={handlechange}/>
+        <button onClick={getweather}>Get Weather</button>
+        {weather &&
+        <> <div className="weatherinfo">
+          <h3>{weather.data.name}</h3>
+          <p>Temp is {weather.data.main.temp}</p>
+          <p>{weather.data.weather[0].description}</p>
+          </div></>}
+      </div>
+    
+        
+  )
+}
+
+export default App
